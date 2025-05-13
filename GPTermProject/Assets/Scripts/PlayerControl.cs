89f0c86543e650d6faef1d9c603bf24c5f2fcd86
futureots <futureots113@gmail.com>
2025-05-13 19:29:32 +0900

    add player control

--- GPTermProject/Assets/Scripts/PlayerControl.cs
+++ GPTermProject/Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+using static UnityEngine.Rendering.DebugUI.Table;
+
 public class PlayerControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -10,6 +12,9 @@ public class PlayerControl : MonoBehaviour
 
     Vector3 direction;
     public float speed;
+    public float rotateSpeed;
+
+    public GameObject cameraObj;
     private void Awake()
     {
         _inputActions = new InputSystem_Actions();
@@ -19,23 +24,41 @@ public class PlayerControl : MonoBehaviour
         _inputActions.Player.Attack.performed += value =>
         {
             Debug.Log("Throw");
-            var cake = Instantiate(cupCake,transform.position+ Vector3.forward,transform.rotation);
+            var cake = Instantiate(cupCake,transform.position+ cameraObj.transform.forward,transform.rotation);
             var rigidbody = cake.GetComponent<Rigidbody>();
-            rigidbody.AddForce(transform.forward*power, ForceMode.Impulse);
+            rigidbody.AddForce(cameraObj.transform.forward*power, ForceMode.Impulse);
         };
-
+        _inputActions.Player.Move.performed += value =>
+        {
+            Vector2 vec = value.ReadValue<Vector2>();
+            Debug.Log(vec);
+            direction = new Vector3(vec.x, 0, vec.y);
+        };
+        _inputActions.Player.Move.canceled += value => direction = Vector3.zero;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
-        
+        SetCamera();
 
-        transform.Translate(direction * speed * Time.deltaTime);
+        var mouseX = Input.GetAxis("Mouse X");
+        var rot = transform.rotation.eulerAngles;
+        rot.y += mouseX * rotateSpeed * 100 * Time.deltaTime;
 
+        transform.rotation = Quaternion.Euler(rot);
+
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+    }
+
+    void SetCamera()
+    {
+        var mouseY = Input.GetAxis("Mouse Y");
+        var rot = cameraObj.transform.rotation.eulerAngles;
+        rot.x -= mouseY * rotateSpeed * 100 * Time.deltaTime;
+        cameraObj.transform.rotation = Quaternion.Euler(rot);
     }
     private void OnEnable()
     {
