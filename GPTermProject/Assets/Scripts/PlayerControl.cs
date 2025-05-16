24218f555f4050cfde7769aafb6085e11a804d22
futureots <futureots113@gmail.com>
2025-05-16 22:54:58 +0900

    feat : player move & camera setting

--- GPTermProject/Assets/Scripts/PlayerControl.cs
+++ GPTermProject/Assets/Scripts/PlayerControl.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -5,22 +7,70 @@ using UnityEngine.InputSystem;
 public class PlayerControl : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public static InputSystem_Actions _inputActions;
+    InputSystem_Actions _inputActions;
     public GameObject cupCake;
-    
+
+    public Transform rHand;
+    public Animator anim;
+
+    public float speed;
+    public float rotateSpeed;
+    Vector3 direction;
+    public float power;
     private void Awake()
     {
         _inputActions = new InputSystem_Actions();
-    }
-    void Start()
-    {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
+    bool isThrowing = false;
+    private void Start()
+    {
+        _inputActions.Player.Attack.performed += value =>
+        {
+            Debug.Log("Throw");
+            if (!isThrowing)
+            {
+                StartCoroutine(ThrowCoroutine());
+            }
+        };
+        _inputActions.Player.Move.started += value => anim.SetBool("IsRun", true);
+        _inputActions.Player.Move.performed += value =>
+        {
+            var vec = value.ReadValue<Vector2>();
+            direction.x = vec.x;
+            direction.z = vec.y;
+            direction.Normalize();
+        };
+        _inputActions.Player.Move.canceled += value =>
+        {
+            anim.SetBool("IsRun", false);
+            direction = Vector3.zero;
+        };
+        _inputActions.Player.Look.performed += value =>
+        {
+            var vec = value.ReadValue<Vector2>();
+            transform.Rotate(Vector3.up * vec.x * rotateSpeed * 0.1f);
+        };
+    }
+
+
+
+    IEnumerator ThrowCoroutine()
+    {
+        anim.SetTrigger("Throw");
+        //isThrowing = true;  
+        yield return new WaitForSeconds(0.65f);
+        var cake = Instantiate(cupCake, rHand.position, rHand.rotation);
+        var rigidbody = cake.GetComponent<Rigidbody>();
+        rigidbody.AddForce((transform.forward + Vector3.up).normalized * power, ForceMode.Impulse);
+        yield return new WaitForSeconds(1.5f);
+        //isThrowing = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(direction * Time.deltaTime * speed);
     }
     private void OnEnable()
     {
